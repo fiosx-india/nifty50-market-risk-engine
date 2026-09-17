8157cd2ef167927852813d6f68decca3131b0ad0
fiosx-india <sharvikaenterprises2015@gmail.com>
2026-09-17 14:49:38 +0530

    Add files via upload

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,1 +1,7 @@
-
+"""Smoke tests for the new calculation layer."""
+def test_candlestick():
+    from indicators.candlestick_patterns import doji
+    assert doji(100,105,95,100)
+def test_returns():
+    from historical.return_calculator import simple_returns
+    assert simple_returns([100,110]) == [0.1]
